03b6865a1fd81dcb2e7259c674d5ca128c55d9f3
Ghost620 <faghost6201@gmail.com>
2022-09-16 15:10:29 +0500

    Create firebase.js

---
+++ ig-reels-clone/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAKw3HJZkT58dlo_dGtt2wUFlQpe8poV0I",
+    authDomain: "instagram-reels-clone-react.firebaseapp.com",
+    projectId: "instagram-reels-clone-react",
+    storageBucket: "instagram-reels-clone-react.appspot.com",
+    messagingSenderId: "149645612557",
+    appId: "1:149645612557:web:d8f3a639b4d715a1ee6744",
+    measurementId: "G-HREBFREZ5H"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+
+  export default db
